9dc8a898ba9751bd2c1d48982a6b47c2b5674f58
Dennis Ouyang <den2net@yahoo.com>
2017-05-10 02:28:20 +0000

    added bootstrap and changed files according to docs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require jquery
+//= require tether
+//= require bootstrap-sprockets
+//= require_tree 
+
+
