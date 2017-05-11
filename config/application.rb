cd569ce104a2bd1bb6256d62d5468a1e8d221111
Dennis Ouyang <den2net@yahoo.com>
2017-05-11 00:23:09 +0000

    styling top nav

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module FlixterDennisOuyang
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Adding fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
