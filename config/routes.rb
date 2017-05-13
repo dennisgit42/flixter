78f6867f6c06b73d331b636ac8e929be25502553
Dennis Ouyang <den2net@yahoo.com>
2017-05-13 22:24:22 +0000

    Added sections to courses

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root "static_pages#index"
   
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
   end
 
   resources :courses, only: [:index, :show]
