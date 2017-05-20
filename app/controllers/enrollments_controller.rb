f3816e693cc405c9f4b0aa5dfe4ac8e95b06f03c
Dennis Ouyang <den2net@yahoo.com>
2017-05-20 02:59:32 +0000

    Setup credit card payments with Stripe

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,27 +2,29 @@ class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    current_user.enrollments.create(course: current_course)
-
     # Amount in cents
-    @amount = 500
+    if current_course.premium?
+      @amount = (current_course.cost * 100).to_i
 
-    customer = Stripe::Customer.create(
-      email: params[:stripeEmail],
-      source:  params[:stripeToken]
-    )
+      customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source:  params[:stripeToken]
+      )
 
-    charge = Stripe::Charge.create(
-      customer:  customer.id,
-      amount:    @amount,
-      description: 'Rails Stripe customer',
-      currency:    'usd'
-    )
+      charge = Stripe::Charge.create(
+        customer:  customer.id,
+        amount:    @amount,
+        description: 'Flixter Premium Content',
+        currency:    'usd'
+      )
+    end  
 
+  current_user.enrollments.create(course: current_course)
   redirect_to course_path(current_course) 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to root_path
+
   end
 
   private
