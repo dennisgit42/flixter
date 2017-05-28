a28748319e7bfbf3ba0989ee26c710410de3a1ed
Dennis Ouyang <den2net@yahoo.com>
2017-05-28 00:18:58 +0000

    Believe I finally fixed ActionController::InvalidAuthenticityToken Error

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -2,6 +2,7 @@ class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_section, only: [:create]
   before_action :require_authorized_for_current_lesson, only: [:update]
+  skip_before_action :verify_authenticity_token
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
